5814820429482d33b52184ee9d6c22ad6685cbbe
Tamara Khalbashkeyeva <infinityxxx@gmail.com>
2014-06-27 23:48:32 +0900

    Fix for default settings value

--- thumbnail_maker/settings.py
+++ thumbnail_maker/settings.py
@@ -4,6 +4,6 @@ Settings
 from django.conf import settings
 
 
-THUMBNAIL_MAKER_FORMATS = getattr(settings, 'THUMBNAIL_MAKER_FORMATS', ())
+THUMBNAIL_MAKER_FORMATS = getattr(settings, 'THUMBNAIL_MAKER_FORMATS', {})
 
 THUMBNAIL_MAKER_DEBUG = getattr(settings, 'THUMBNAIL_MAKER_DEBUG', False)
