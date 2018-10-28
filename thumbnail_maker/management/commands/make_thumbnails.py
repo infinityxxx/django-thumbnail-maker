8f0ca8563ef3f91b6ad0ac3a0f554f80b076a569
Tamara Khalbashkeyeva <infinityxxx@gmail.com>
2018-10-28 18:43:13 +0200

    Django 1.10: utilize Command.add_arguments

--- thumbnail_maker/management/commands/make_thumbnails.py
+++ thumbnail_maker/management/commands/make_thumbnails.py
@@ -4,53 +4,52 @@ Additional commands for thumbnails
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.contenttypes.models import ContentType
 
-from optparse import make_option
-
 
 class Command(BaseCommand):
-    args = "<app.model> <field>"
-    option_list = BaseCommand.option_list + (
-        make_option('--force', '-f', action='store_true',
-                    dest='force', default=False,
-                    help='Force update of thumbnails'),
-    )
     help = "Auto-generate thumbnails for all instances of " \
            "the given model for the given field."
+
+    def add_arguments(self, parser):
+        """Required and optional arguments."""
+        # required arguments:
+        parser.add_argument('app_model', type=str)
+        parser.add_argument('field', type=str)
+
+        # optional arguments:
+        parser.add_argument(
+            '-f',
+            '--force',
+            action='store_true',
+            dest='force',
+            default=False,
+            help='Force update of thumbnails',
+        )
 
     def handle(self, *args, **options):
         self.args = args
         self.force = options.get('force')
-        self.validate_input()
-        self.parse_input()
+        self.model = self.get_model(options['app_model'])
+        self.field = options['field']
         self.make_thumbnails(force=self.force)
 
-    def validate_input(self):
-        num_args = len(self.args)
-
-        if num_args < 2:
-            raise CommandError("Please pass `app.model` and `field`")
-        if num_args > 2:
-            raise CommandError("Too many arguments")
-
-        if '.' not in self.args[0]:
+    def get_model(self, app_model):
+        """Parse `app_model` string and return model class."""
+        try:
+            app, model_name = app_model.split('.')
+        except ValueError:
             raise CommandError("The first argument must be: app.model")
 
-    def parse_input(self):
-        """
-        Get input parameters
-        """
-        app, model_name = self.args[0].split('.')
         model_name = model_name.lower()
 
         try:
-            self.model = ContentType.objects.get(
+            model = ContentType.objects.get(
                 app_label=app, model=model_name
             )
-            self.model = self.model.model_class()
+            model = model.model_class()
         except ContentType.DoesNotExist:
-            raise CommandError("Not found app/model: %s" % self.args[0])
-
-        self.field = self.args[1]
+            raise CommandError("Not found app/model: {}".format(app_model))
+        else:
+            return model
 
     def make_thumbnails(self, force=False):
         """
